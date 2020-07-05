1a8838a23b2887f5f0db866aef8a60aa030ad946
Yuichi Nakamura <fyuich@gmail.com>
2020-07-05 15:05:05 +0900

    矢印キーで機体を動かせるようにする

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@
   const CANVAS_HEIGHT = 480;
 
   let startTime = null;
+  let viperX = CANVAS_WIDTH / 2;
+  let viperY = CANVAS_HEIGHT / 2;
 
   window.addEventListener('load', () => {
     util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
@@ -12,6 +14,7 @@
     util.imageLoader('./image/viper.png', (loadedImage) => {
       image = loadedImage;
       initialize();
+      eventSetting();
       startTime = Date.now();
       render();
     });
@@ -29,9 +32,27 @@
     util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
 
     let nowTime = (Date.now() - startTime) / 1000;
-    let s = Math.sin(nowTime);
-    let x = s * 100.0;
-    ctx.drawImage(image, CANVAS_WIDTH / 2 + x, CANVAS_HEIGHT / 2);
+    ctx.drawImage(image, viperX, viperY);
     requestAnimationFrame(render);
+  }
+
+  // イベントを設定する
+  function eventSetting() {
+    window.addEventListener('keydown', (event) => {
+      switch(event.key) {
+        case 'ArrowLeft':
+          viperX -= 10;
+          break;
+        case 'ArrowRight':
+          viperX += 10;
+          break;
+        case 'ArrowUp':
+          viperY -= 10;
+          break;
+        case 'ArrowDown':
+          viperY += 10;
+          break;
+      }
+    }, false);
   }
 })();
