a99417eeb7c539a05f9b970438fad5f3baa7f16c
Yuichi Nakamura <fyuich@gmail.com>
2020-07-05 15:22:41 +0900

    自機の登場シーンを作る

--- script/script.js
+++ script/script.js
@@ -3,8 +3,15 @@
   const CANVAS_HEIGHT = 480;
 
   let startTime = null;
+
+  // 自機の座標
   let viperX = CANVAS_WIDTH / 2;
   let viperY = CANVAS_HEIGHT / 2;
+
+  // 自機が登場中かどうかを表すフラグ
+  let isComing = false;
+  // 登場演出を開始した際のタイムスタンプ
+  let comingStart = null;
 
   window.addEventListener('load', () => {
     util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
@@ -24,14 +31,34 @@
   function initialize() {
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
+
+    // 登場シーンからスタートするための設定
+    isComing = true;
+    comingStart = Date.now();
+    viperY = CANVAS_HEIGHT;
   }
 
   // 描画処理を行なう
   function render() {
+    ctx.globalAlpha = 1.0;
     // 描画前に画面全体を不透明な明るいグレーで塗りつぶす
     util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
 
     let nowTime = (Date.now() - startTime) / 1000;
+
+    if(isComing === true) {
+      let justTime = Date.now();
+      let comingTime = (justTime - comingStart) / 1000;
+      viperY = CANVAS_HEIGHT - comingTime * 50;
+      if(viperY <= CANVAS_HEIGHT -100) {
+        isComing = false;
+        viperY = CANVAS_HEIGHT - 100;
+      }
+      if(justTime % 100 < 50) {
+        ctx.globalAlpha = 0.5;
+      }
+    }
+
     ctx.drawImage(image, viperX, viperY);
     requestAnimationFrame(render);
   }
@@ -39,6 +66,8 @@
   // イベントを設定する
   function eventSetting() {
     window.addEventListener('keydown', (event) => {
+      // 登場シーンなら何もしないで終了
+      if(isComing === true) {return;}
       switch(event.key) {
         case 'ArrowLeft':
           viperX -= 10;
