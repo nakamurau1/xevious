5459752c23d28b72c4ee7a7f3b929c9248109514
Yuichi Nakamura <fyuich@gmail.com>
2020-07-16 20:33:24 +0900

    ショットのベクトルを変更可能にする

--- script/character.js
+++ script/character.js
@@ -126,6 +126,7 @@ class Shot extends Character {
     super(ctx, x, y, w, h, 0, imagePath);
 
     this.speed = 7;
+    this.vector = new Position(0.0, -1.0);
   }
 
   set(x, y) {
@@ -133,12 +134,17 @@ class Shot extends Character {
     this.life = 1;
   }
 
+  setVector(x, y) {
+    this.vector.set(x, y);
+  }
+
   update() {
     if(this.life <= 0) {return;}
     if(this.position.y + this.height < 0) {
       this.life = 0;
     }
-    this.position.y -= this.speed;
+    this.position.x += this.vector.x * this.speed;
+    this.position.y += this.vector.y * this.speed ;
     this.draw();
   }
 }
